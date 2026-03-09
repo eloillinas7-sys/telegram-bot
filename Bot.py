2a33c1d02aca65c83d57e86655ae430bd210dadb
eloillinas7-sys <eloillinas7@gmail.com>
2026-03-09 18:36:21 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,5 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from datetime import time
+import random
 import os
 
 TOKEN = os.environ["BOT_TOKEN"]
